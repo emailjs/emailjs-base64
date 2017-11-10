d1c406d6c4095ce7caab4b3a447ec78cf96255a4
Felix Hammerl <fhammerl@thoughtworks.com>
2017-11-10 16:23:10 +0100

    Updating dist files

---
+++ dist/base64.js
@@ -0,0 +1,26 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _base64Encode = require('./base64-encode');
+
+Object.defineProperty(exports, 'encode', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_base64Encode).default;
+  }
+});
+
+var _base64Decode = require('./base64-decode');
+
+Object.defineProperty(exports, 'decode', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_base64Decode).default;
+  }
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9iYXNlNjQuanMiXSwibmFtZXMiOlsiZGVmYXVsdCJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7aURBQVNBLE87Ozs7Ozs7OztpREFDQUEsTyIsImZpbGUiOiJiYXNlNjQuanMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgeyBkZWZhdWx0IGFzIGVuY29kZSB9IGZyb20gJy4vYmFzZTY0LWVuY29kZSdcbmV4cG9ydCB7IGRlZmF1bHQgYXMgZGVjb2RlIH0gZnJvbSAnLi9iYXNlNjQtZGVjb2RlJ1xuIl19
